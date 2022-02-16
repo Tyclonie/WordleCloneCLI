5af8e66aebd8e2870df74f7a8a36269bb04abc20
williamthomson <mcpeclasher@gmail.com>
2022-02-16 13:41:25 +0000

    test

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Wordle 
+{
+    class Program 
+    {
+        static void Main(String[] args) 
+        {
+            var game = new Game("hello");
+            Console.WriteLine(game.word);
+        }
+    }
+}
---
+++ game.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Wordle
+{
+    public class Game
+    {
+        public String word;
+        public Game(String word) 
+        {
+            this.word = word;
+        }
+    }
+}
